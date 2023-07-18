b43ff663cd16ded3771e0671b7510d59332fc9c6
I-amour <110339182+I-amour@users.noreply.github.com>
2023-07-18 16:58:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-// Emoticon Animation on Scroll
-const emoticons = document.querySelectorAll('.emoticon');
-
-window.addEventListener('scroll', () => {
-  const scrollPosition = window.scrollY;
-  emoticons.forEach((emoticon) => {
-    const offsetTop = emoticon.offsetTop;
-    if (scrollPosition > offsetTop - window.innerHeight + 100) {
-      emoticon.classList.add('animate');
-    }
+// script.js
+window.addEventListener('DOMContentLoaded', function() {
+    const popup = document.getElementById('popup');
+    const closeBtn = document.getElementById('close-btn');
+    
+    closeBtn.addEventListener('click', function() {
+      popup.style.display = 'none';
+    });
   });
-});
+  
