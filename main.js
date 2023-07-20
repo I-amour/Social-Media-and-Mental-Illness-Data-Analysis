28bc17eb4c3c4b8f4c8e17845e6686ac4ccdd43b
I-amour <110339182+I-amour@users.noreply.github.com>
2023-07-20 12:06:38 +0100

    Scroll back up button

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-// script.js
-window.addEventListener('DOMContentLoaded', function() {
-    const popup = document.getElementById('popup');
-    const closeBtn = document.getElementById('close-btn');
-    
-    closeBtn.addEventListener('click', function() {
-      popup.style.display = 'none';
-    });
-  });
+// Show or hide the Back to Top button based on scroll position
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("backToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("backToTopBtn").style.display = "none";
+  }
+};
+
+// Scroll to the top when the Back to Top button is clicked
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
   
